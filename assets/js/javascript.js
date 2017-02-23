e5aa1b58d81c76dae2e5414bba520f3048e04574
Janine Vega <osmary.vega@gmail.com>
2017-02-23 18:19:26 -0500

    Validacion y reset input

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,14 +1,22 @@
 
-function capturar(){
+var en = document.getElementById('enviar');
+en.onclick=function (){
 
   var nombre    = document.getElementById('nombre').value;
   var apellido  = document.getElementById('apellido').value;
   var dni       = document.getElementById('dni').value;
   var direccion = document.getElementById('direccion').value;
-  var records   = document.getElementById('records');
+  var printAll   = document.getElementById('printAll');
+  var formulario= document.getElementsByName('formulario')[0];
 
-  records.innerHTML=( "<li>Nombre:"     + nombre      + "<br>"   +
+if(nombre !="" && apellido !="" && dni !="" && direccion !="" ){
+  printAll.innerHTML=( "<li>Nombre:"     + nombre      + "<br>"   +
                       "<li>Apellidos: " + apellido    + "<br>"   +
                       "<li>DNI: "       + dni         + "<br>"   +
                       "<li>Direccion: " + direccion);
+
+formulario.reset();
+}
+
+
 }

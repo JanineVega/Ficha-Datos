9da4be49812d9b044d8f163cc682c30c476ff32b
Janine Vega <osmary.vega@gmail.com>
2017-03-27 09:02:25 -0500

    levanano observaciones

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,22 +1,25 @@
 
 var en = document.getElementById('enviar');
 en.onclick=function (){
+  if(document.getElementsByTagName("input").length==0){
+    alert("Porfavor ingrese los datos solicitados");+
+    console.log(document.getElementsByTagName("input").length==0);
+  }
 
-  var nombre    = document.getElementById('nombre').value;
-  var apellido  = document.getElementById('apellido').value;
-  var dni       = document.getElementById('dni').value;
-  var direccion = document.getElementById('direccion').value;
-  var printAll   = document.getElementById('printAll');
-  var formulario= document.getElementsByName('formulario')[0];
+  var nombre      = document.getElementById('nombre').value;
+  var apellido    = document.getElementById('apellido').value;
+  var dni         = document.getElementById('dni').value;
+  var direccion   = document.getElementById('direccion').value;
+  var printAll    = document.getElementById('printAll');
+  var formulario  = document.getElementsByName('formulario')[0];
 
 if(nombre !="" && apellido !="" && dni !="" && direccion !="" ){
-  printAll.innerHTML=( "<li>Nombre:"     + nombre      + "<br>"   +
+  printAll.innerHTML=( "<ul><li>Nombre:"     + nombre      + "<br>"   +
                       "<li>Apellidos: " + apellido    + "<br>"   +
                       "<li>DNI: "       + dni         + "<br>"   +
-                      "<li>Direccion: " + direccion);
+                      "<li>Direccion: " + direccion)+"<ul>";
 
 formulario.reset();
 }
-console.log("Hola Mundo");
 
 }
